caf83ece22d3db358f2bdaa5de25cf3bde6baf38
Michael Wu <michaelwu@Michaels-MacBook-Pro.local>
2019-01-30 09:14:14 -0800

    new results added,
    micro expression vs Gesture in VR
    
    Micro expression vs Headpose in VR
    
    more in twoD, please see file

---
+++ drawMicroGestureVR.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+M = []
+G = []
+S_VR = []
+for m in range(0,100):
+    for g in range(0,100):
+        for s_vr in range(100):
+            if(2 * m + g - s_vr <= 70 and 2 * m + g - s_vr >= 68):
+                M.append(m)
+                G.append(g)
+                S_VR.append(s_vr)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(M,G,S_VR)
+ax.set_xlabel("MicroExpression")
+ax.set_ylabel("Gesture")
+ax.set_zlabel("Virtural Reality")
+plt.show()
---
+++ drawMicroHeadVR.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+M = []
+H = []
+S_VR = []
+for m in range(0,100):
+    for h in range(0,100):
+        for s_vr in range(100):
+            if(m-h-s_vr >= 11 and m-h-s_vr <=12):
+                M.append(m)
+                H.append(h)
+                S_VR.append(s_vr)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(M,H,S_VR)
+ax.set_xlabel("X = MicroExpression")
+ax.set_ylabel("Y = Gesture")
+ax.set_zlabel("Z = Virtural Reality")
+plt.show()
